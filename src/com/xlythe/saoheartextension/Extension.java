d28d5fdf85ea70d63310a67b1ea177340664e14b
Xlythe <xlythe@gmail.com>
2013-11-17 00:42:53 -0500

    added more methods, no longer static

--- src/com/xlythe/saoheartextension/Extension.java
+++ src/com/xlythe/saoheartextension/Extension.java
@@ -5,13 +5,19 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class Extension {
-    public static View getView(Context context) {
+    public View getView(Context context) {
         ImageView iv = new ImageView(context);
         iv.setImageResource(R.drawable.heart);
         return iv;
     }
 
-    public static String getImage() {
+    public String getImage() {
         return "icon_heart";
     }
+
+    public void setButtonPos(int[] pos) {}
+
+    public void viewShown() {}
+
+    public void viewHidden() {}
 }
